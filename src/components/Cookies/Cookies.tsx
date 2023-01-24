607b253a380416d4855fc48b9d2c893c0377d06a
Wojciech Pawlicki-Gil <voypgi@gmail.com>
2023-01-24 19:19:13 +0100

    new pages, pagination fixed, added components to /home

--- src/components/Cookies/Cookies.tsx
+++ src/components/Cookies/Cookies.tsx
@@ -1,12 +1,25 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import "./Cookies.css"
 
 export const Cookies = () => {
-    const [consent, setConsent] = useState<boolean>(false)
+    const [consent, setConsent] = useState<boolean>(false);
+
+    useEffect(() => {
+        const consentCookie = localStorage.getItem('cookie');
+        if (consentCookie) {
+            setConsent(true);
+        }
+      }, []);
+
+    const setConsentForm = () => {
+        setConsent(true);
+        localStorage.setItem("cookie", "true");
+    }
+
     return (
         <form className="cookie-bar" style={consent ? { display: "none" } : {}}>
             <p>By continuing to browse, you accept the use of third-party cookies intended to offer you services adapted to your browsing and social networks.</p>
-            <button type="button" onClick={() => setConsent(true)}>Accept</button>
+            <button type="button" onClick={() => setConsentForm()}>Accept</button>
         </form>
     );
 }
