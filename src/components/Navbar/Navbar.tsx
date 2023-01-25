f5e9569d3c17bd92f8e64e2d1f26f0fdd542d5ce
Wojciech Pawlicki-Gil <voypgi@gmail.com>
2023-01-25 01:19:55 +0100

    complete redesign of navbar to modern style

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,21 +1,67 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'
 
 
 
 export const Navbar = () => {
+    const [activeLink, setActiveLink] = useState("home");
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const handleScroll = () => {
+        if (window.scrollY > 800) {
+            setScrolled(true);
+        } else {
+            setScrolled(false);
+        }
+    };
+
+
+    const navElements = (
+        <div className={`navbar-list ${scrolled ? "scrolled" : ""}`}>
+            <header className='nav-title'>
+                <Link to='/'
+                    className={activeLink === "home" ? "active" : ""}
+                    onClick={() => setActiveLink("home")}
+                ><h1>Hotello</h1></Link>
+            </header>
+            <ul >
+                <li
+                    className={activeLink === "home" ? "active" : ""}
+                    onClick={() => setActiveLink("home")}
+                ><Link to='/'>Home</Link></li>
+                <li
+                    className={activeLink === "hotels" ? "active" : ""}
+                    onClick={() => setActiveLink("hotels")}
+                ><Link to='/hotels'>Hotels</Link></li>
+                <li
+                    className={activeLink === "events" ? "active" : ""}
+                    onClick={() => setActiveLink("events")}
+                ><Link to='/events'>Events</Link></li>
+                <li
+                    className={activeLink === "about" ? "active" : ""}
+                    onClick={() => setActiveLink("about")}
+                ><Link to='/about'>About</Link></li>
+                <li
+                    className={activeLink === "contact" ? "active" : ""}
+                    onClick={() => setActiveLink("contact")}
+                ><Link to='/contact'>Contact</Link></li>
+            </ul>
+        </div>
+    )
 
 
     return (
         <nav className='img-div'>
-            <ul className='navbar-list'>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/hotels'>Hotels</Link></li>
-                <li><Link to='/about'>About</Link></li>
-            </ul>
-            <header className='nav-title'>
-                <h1>HotelName</h1>
-            </header>
+            {navElements}
+
             <img alt="background" style={{ display: "none" }}
             //  src='./navImg.jpg'
             ></img>
