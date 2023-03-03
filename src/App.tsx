2f0538ce74f854329e084894cd57353ad14f4393
Wojciech Pawlicki-Gil <voypgi@gmail.com>
2023-03-03 14:39:20 +0100

    scroll to top when changing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom'
-import { About } from './components/About';
+import { About } from './pages/About/About';
 import { Navbar } from "./pages/Navbar/Navbar"
 import { Home } from './pages/Home/Home';
 import { Cookies } from './pages/Cookies/Cookies';
 import { Footer } from './pages/Footer/Footer';
 import { Contact } from './pages/Contact/Contact';
 import { HotelList } from './components/HotelList/HotelList';
+import { ErrorPage } from './components/ErrorPage';
+import ScrollToTop from './features/ScrollToTop';
 
 function App() {
   return (
     <div className='app'>
       <Navbar />
+      <ScrollToTop />
       <Routes>
         <Route path='' element={<Home />} />
         <Route path='about' element={<About />} />
         <Route path='contact' element={<Contact />} />
         <Route path='hotels' element={<HotelList />} />
-        {/* <Route path='*' element={<ErrorPage />} /> */}
+        <Route path='*' element={<ErrorPage />} />
       </Routes>
       <Footer />
       <Cookies />
@@ -26,26 +29,3 @@ function App() {
 }
 
 export default App;
-
-/*
-
-3hotele (fajne zakladeczki taby?)
-semantyka html
-podejscie mobile -> desktop
-
-*/
-
-/*  TODO & FIX
-  navbar for mobile hamburger/anilist?
-  after clicking diffrent navElement changes position
-  style filter hotels
-  one more component on /home
-  zmiana Navbar - oddzielenie img od nav, zeby w contact byl tylko nav
-  footer
-  nav pages
-  single hotel page
-  reservation
-  map intergration?
-
-  contact maybe 2 labels: one for transform over box, 2nd for inside box
-*/
