1e0f91e16d55b23fc0d276ea95616ab5ab2d71bd
Wojciech Pawlicki-Gil <voypgi@gmail.com>
2023-03-09 17:00:50 +0100

    error page

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,10 +1,21 @@
-
+import { MdOutlineErrorOutline } from 'react-icons/md'
+import { CardCarousel } from '../pages/CardCarousel'
+import './errorPage.css'
 export function ErrorPage() {
+
+    const errorText = "page could not be found"
+
     return (
-        <div>
-            <h1>
-                error
-            </h1>
+        <div className="row">
+            <div className='error-div'>
+                <MdOutlineErrorOutline className='icon-large' />
+                <h2 
+                style={{"fontWeight":"bold"}}
+                >{errorText}</h2>
+            </div>
+            <CardCarousel 
+            title='Instead you can explore our destinations'
+            />
         </div>
     )
 }
